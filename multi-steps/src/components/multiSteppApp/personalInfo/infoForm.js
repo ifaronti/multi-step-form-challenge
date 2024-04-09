b6aef2070efc8c96df996064e8f5ae341dd41b33
ifaronti <135629496+ifaronti@users.noreply.github.com>
2024-04-09 12:20:37 +0100

    fine tuning

--- multi-steps/src/components/multiSteppApp/personalInfo/infoForm.js
+++ multi-steps/src/components/multiSteppApp/personalInfo/infoForm.js
@@ -12,7 +12,7 @@ export default function InfoForm({info, onChange, phoneError, emailError, nameEr
     // inputs for some reasons it 
     const infoForm = 
     <div className="relative w-full">
-        <label htmlFor='name' className='font flex w-[100%] max-w-[450px] lg:w-[450px] justify-between capitalize font-Ubuntu-Regular text-sm text-[#02295a]'>
+        <label htmlFor='name' className='font flex w-[100%] mb-[3px] lg:mb-[7px] max-w-[450px] lg:w-[450px] justify-between capitalize font-Ubuntu-Regular text-sm text-[#02295a]'>
             Name
             <p className={`text-red-500 font-Ubuntu-Regular ${nameError ? 'visible': 'invisible'}`}>{errorMsg0}</p>
         </label>
@@ -24,16 +24,16 @@ export default function InfoForm({info, onChange, phoneError, emailError, nameEr
             name='name'
             required
             placeholder='e.g Jalil Karma'
-            className='lg:w-[450px]  w-[100%] max-w-[450px] indent-3 text-[1rem] font-Ubuntu-Regular border-solid outline-none border-[1px] h-[43px] lg:h-[50px] mb-4 lg:mb-6 rounded-[4px] lg:rounded-lg my-0'
+            className='lg:w-[450px]  w-[100%] max-w-[450px] indent-3 text-[1rem] font-Ubuntu-Regular border-solid outline-none border-[1px] h-[43px] lg:h-[50px] mb-4 lg:mb-5 rounded-[4px] cursor-pointer hover:border-[#4703b5] text-[#02295a] lg:rounded-lg my-0'
         />
 
-        <label htmlFor='email' className='capitalize flex w-[100%] max-w-[450px] lg:w-[450px] justify-between font-Ubuntu-Regular text-sm text-[#02295a]'>
+        <label htmlFor='email' className='capitalize flex w-[100%] mb-[3px] lg:mb-[7px] max-w-[450px] lg:w-[450px] justify-between font-Ubuntu-Regular text-sm text-[#02295a]'>
             Email Address
             <p className={`${emailError ? 'visible' : 'invisible'} font-Ubuntu-Regular text-red-500`}>{errorMsg1}</p>
         </label>
         <input 
             type='email' 
-            className={`lg:w-[450px] w-[100%] max-w-[450px] ${emailError ? 'border-red-500' : ''} indent-3 text-[1rem] font-Ubuntu-Regular border-solid mb-4 outline-none border-[1px] h-[43px] lg:h-[50px] lg:mb-6 rounded-[4px] lg:rounded-lg my-0`} 
+            className={`lg:w-[450px] w-[100%] max-w-[450px] ${emailError ? 'border-red-500' : ''} indent-3 text-[1rem] font-Ubuntu-Regular border-solid mb-4 outline-none border-[1px] h-[43px] lg:h-[50px] lg:mb-5 rounded-[4px] cursor-pointer hover:border-[#4703b5] text-[#02295a] lg:rounded-lg my-0`} 
             id='email' value={info.email} 
             onChange={onChange} 
             name='email'
@@ -41,7 +41,7 @@ export default function InfoForm({info, onChange, phoneError, emailError, nameEr
             placeholder='Enter your email address'
         />
 
-        <label htmlFor='phone' className='font flex justify-between w-[100%] max-w-[450px] lg:w-[450px] capitalize font-Ubuntu-Regular text-sm text-[#02295a]'>
+        <label htmlFor='phone' className='font flex justify-between mb-[3px] lg:mb-[7px] w-[100%] max-w-[450px] lg:w-[450px] capitalize font-Ubuntu-Regular text-sm text-[#02295a]'>
             Phone Number
             <p className={`${phoneError ? 'visible' : 'invisible'} text-red-500`}>{errorMsg2}</p>
         </label>
@@ -53,7 +53,7 @@ export default function InfoForm({info, onChange, phoneError, emailError, nameEr
             name='phone'
             required
             placeholder='Enter your phone number'
-            className={`lg:w-[450px] w-[100%] max-w-[450px] ${phoneError ? 'border-red-500' : ''} mb-8 indent-3 text-[1rem] font-Ubuntu-Regular border-solid outline-none border-[1px] h-[43px] lg:h-[50px] lg:mb-6 rounded-[4px] lg:rounded-lg my-0`}
+            className={`lg:w-[450px] w-[100%] max-w-[450px] ${phoneError ? 'border-red-500' : ''} mb-8 indent-3 text-[1rem] font-Ubuntu-Regular border-solid outline-none border-[1px] h-[43px] lg:h-[50px] lg:mb-5 rounded-[4px] cursor-pointer hover:border-[#4703b5] text-[#02295a] lg:rounded-lg my-0`}
         />    
     </div>
 
